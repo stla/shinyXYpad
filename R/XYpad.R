87e7c302237f12a7ebbc07742c37554e6aeeb430
stla <laurent_step@yahoo.fr>
2022-09-23 11:35:27 +0200

    refreshed everything

--- R/XYpad.R
+++ R/XYpad.R
@@ -1,5 +1,5 @@
 #' XY pad controller
-#' @description Create a XY pad controller.
+#' @description Creates a XY pad controller to be included in a Shiny UI.
 #'
 #' @param inputId the input slot that will be used to access the value
 #' @param label label for the XY pad, or \code{NULL} for no label
@@ -7,13 +7,13 @@
 #' @param xmin,xmax minimal x-value and maximal x-value
 #' @param ymin,ymax minimal y-value and maximal y-value
 #' @param ndecimals number of decimals of the displayed coordinates (if \code{displayXY=TRUE})
-#' @param width width in pixels
-#' @param height height in pixels
+#' @param width a positive number, the width in pixels
+#' @param height a positive number, the height in pixels
 #' @param bgColor background color, a HTML color
 #' @param xyColor color of the labels of the coordinates (if \code{displayXY=TRUE}), a HTML color
 #' @param xySize font size of the labels of the coordinates (if \code{displayXY=TRUE})
 #' @param xyStyle font style of the labels of the coordinates (if \code{displayXY=TRUE}),
-#' \code{"normal"}, \code{"italic"} or \code{"oblique"}
+#'   \code{"normal"}, \code{"italic"} or \code{"oblique"}
 #' @param coordsColor color of the displayed coordinates (if \code{displayXY=TRUE}), a HTML color
 #' @param pointColor color of the point, a HTML color
 #' @param pointRadius radius of the point in pixels
@@ -23,32 +23,43 @@
 #' @param displayPrevious logical, whether to display the previous position of the point
 #' @param displayXY logical, whether to display the coordinates
 #' @param onMove logical, whether to send value to server on mouse move
+#'   (\code{TRUE}) or on mouse release (\code{FALSE})
 #'
-#' @return A XY pad input control that can be added to a UI definition.
+#' @return A \code{shiny.tag.list} object generating a XY pad input control that
+#'   can be added to a Shiny UI definition.
 #' @seealso \code{\link{updateXYpadInput}} for updating the XY pad on server-side.
 #' @export
 #' @import shiny
 #'
 #' @examples
-#' if(interactive()){
 #' library(shiny)
 #' library(shinyXYpad)
 #' ui <- fluidPage(
 #'   fluidRow(
-#'     column(6, XYpadInput("xy1", onMove = TRUE, label = "XY pad")),
-#'     column(6, XYpadInput("xy2", label = "XY pad", displayXY = FALSE,
-#'                          displayPrevious = FALSE))
+#'     column(
+#'       6,
+#'       XYpadInput("xy1", onMove = TRUE, label = "XY pad - on move")
+#'     ),
+#'     column(
+#'       6,
+#'       XYpadInput(
+#'         "xy2", label = "XY pad - on release",
+#'         displayXY = FALSE, displayPrevious = FALSE
+#'       )
+#'     )
 #'   ),
 #'   fluidRow(
 #'     column(6, verbatimTextOutput("xy1value")),
 #'     column(6, verbatimTextOutput("xy2value"))
 #'   )
 #' )
-#' server <- function(input, output){
+#' server <- function(input, output, session){
 #'   output[["xy1value"]] <- renderPrint({ input[["xy1"]] })
 #'   output[["xy2value"]] <- renderPrint({ input[["xy2"]] })
 #' }
-#' shinyApp(ui, server)
+#'
+#' if(interactive()){
+#'   shinyApp(ui, server)
 #' }
 XYpadInput <- function(inputId, label = NULL, value = list(x=50, y=50),
                        xmin=0, xmax=100, ymin=0, ymax=100, ndecimals = 2,
@@ -110,9 +121,9 @@ XYpadInput <- function(inputId, label = NULL, value = list(x=50, y=50),
 
 
 #' Change a XY pad input on the client
-#' @description Change a XY pad input on the client.
+#' @description Changes a XY pad input on the client.
 #'
-#' @param session session object passed to function given to shinyServer
+#' @param session session object passed to the Shiny server function
 #' @param inputId id of the XY pad input
 #' @param label new label, or \code{NULL} for no change
 #' @param value new value, or \code{NULL} for no change
@@ -128,7 +139,6 @@ XYpadInput <- function(inputId, label = NULL, value = list(x=50, y=50),
 #' @export
 #'
 #' @examples
-#' if(interactive()){
 #' library(shiny)
 #' library(shinyXYpad)
 #' ui <- fluidPage(
@@ -147,7 +157,9 @@ XYpadInput <- function(inputId, label = NULL, value = list(x=50, y=50),
 #'                      pointRadius = 10, ndecimals = 3)
 #'   })
 #' }
-#' shinyApp(ui, server)
+#'
+#' if(interactive()){
+#'   shinyApp(ui, server)
 #' }
 updateXYpadInput <- function(session, inputId, label = NULL, value = NULL,
                              xmin = NULL, xmax = NULL,
--- R/internal.R
+++ R/internal.R
@@ -1,3 +1,3 @@
 dropNulls <- function(x){
-  x[!vapply(x, is.null, FUN.VALUE = logical(1))]
+  x[!vapply(x, is.null, FUN.VALUE = logical(1L))]
 }
